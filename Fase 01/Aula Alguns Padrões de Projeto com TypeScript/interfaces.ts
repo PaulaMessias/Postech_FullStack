1bb9a8297c8007d5ec728a8c45a4f01152b46607
Paula Messias <paulasm.dev@gmail.com>
2026-03-19 23:55:19 -0300

    teste

---
+++ Fase 01/Aula Alguns Padrões de Projeto com TypeScript/interfaces.ts
@@ -0,0 +1,36 @@
+interface Veiculo {
+  marca: string;
+  modelo: string;
+
+  ligar(): void;
+  acelerar (velocidade: number): void;
+}
+
+class Carro implements Veiculo {
+  marca: string;
+  modelo: string;
+  private ligado: boolean = false;
+
+  constructor(marca: string, modelo: string) {
+    this.marca = marca;
+    this.modelo = modelo;
+  }
+
+  ligar(): void {
+    this.ligado = true;
+    console.log('$(this.marca) $(this.modelo) ligado.');
+
+  }
+
+  ligar(): void {
+    this.ligado = true;
+    console.log('$(this.marca) $(this.modelo) ligado.');
+  }
+  acelerar(velocidade: number): void {
+      if (!this.ligado) {
+        console.log('$(this.marca) $(this.modelo) está desligado.');
+        return;
+      }
+      console.log('$(this.marca) $(this.modelo) acelerando a $(velocidade) km/h.');
+  }
+}
